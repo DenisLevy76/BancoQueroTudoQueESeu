82b2de123d372b17af450d66ce409f4d7ca45dd3
Denis Levy <denis.levi11@gmail.com>
2020-09-22 01:12:57 -0300

    consertando a aplicação

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function iniciarModal(modalID) {
    }
 }
 
-const popup = document.querySelector('#modalText')
+
 const botao = document.querySelector('.avaliar')
 const form = document.querySelector('#form1')
 
@@ -18,49 +18,52 @@ let polp = 'inadequada'
 let rend = 'inadequada'
 let invest = 'Poupança'
 
-form.addEventListener("submit", function (event) {
-   event.preventDefault()
+function validar() {
+   document.getElementById('form1').addEventListener('click', event => {event.preventDefault()}, false)
+   
+   const nome = document.getElementById("nome").value
+   const valorEmPolpanca = Number(document.getElementById("valorEmPolpanca").value)
+   const rendaMensal = Number(document.getElementById('rendaMensal').value)
+   const numeroDeDependentes = Number(document.getElementById('numeroDeDependentes').value)
 
-   var dados = {
-      nome: forms1.nome.value,
-      valorEmPolpanca: Number(forms1.valorEmPolpanca.value),
-      rendaie: forms1.rendaie.value,
-      rendaMensal: Number(forms1.rendaMensal.value),
-      numeroDeDependentes: Number(forms1.numeroDeDependentes.value),
+
+   const renei = document.getElementsByName('rendaie')
+   const rendaie = renei[0].checked? renei[0].value : renei[1].value
+
+   let polp = 'inadequada'
+   let rend = 'inadequada'
+   let invest = 'Poupança'
+
+
+   if (valorEmPolpanca > 5000 * numeroDeDependentes) {
+      polp = 'adequada'
    }
-   console.log(dados);
-
-   function validar() {
-
-      if (dados.numeroDeDependentes !== '' || dados.nome !== '' && dados.valorEmPolpanca === Number && dados.rendaMensal === Number && dados.numeroDeDependentes === Number) {
-
-         if (dados.valorEmPolpanca > 5000 * dados.numeroDeDependentes) {
-            polp = 'adequada'
-         }
-         if (dados.rendaMensal > 15000 + (4000 * dados.numeroDeDependentes) && dados.rendaie === 'estavel') {
-            rend = 'adequada'
-         }
-
-         if (polp === 'adequada' && rend === 'adequada') {
-            invest = 'Ações'
-         } else if (polp === 'adequada' && rend === 'inadequada') {
-            invest = 'Combinado'
-         }
-
-      }
+   if (rendaMensal > 15000 + (4000 * numeroDeDependentes) && rendaie === 'estavel') {
+      rend = 'adequada'
    }
 
+   if (polp === 'adequada' && rend === 'adequada') {
+      invest = 'Ações'
+
+   } else if (polp === 'adequada' && rend === 'inadequada') {
+      invest = 'Combinado'
+   }
+
+   console.log(nome, valorEmPolpanca, rendaMensal, rendaie, numeroDeDependentes)
+   const popup = document.querySelector('#modalText')
 
 
-
-   botao.addEventListener('click', () => {
-      validar()
-      popup.innerHTML = `<h1>Olá ${dados.nome}, belezinha?!</h1>
-      <p>Analisando seus dados recomendamos fortemente nosso plano "<b>${invest}</b>".</p>`
-      console.log(invest);
+   popup.innerHTML = `<h1>Olá ${nome}, belezinha?!</h1>
+   <p>Analisando seus dados recomendamos fortemente nosso plano "<b>${invest}</b>".</p>`
+   
+   if (numeroDeDependentes != '') {
+      
       iniciarModal('#ModalContainer')
-      form.values = ''
-   })
+   }
+}
 
 
-})
+function modal(){
+
+   validar()
+}
